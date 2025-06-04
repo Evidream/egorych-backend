48634ac15c9b4834db8ac47990be3008bf6dc80f
Evidream <e.v.i.dream@mail.ru>
2025-06-04 19:30:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -50,8 +50,7 @@ app.post("/speak", async (req, res) => {
         model_id: "eleven_multilingual_v2",
         voice_settings: {
           stability: 0.3,
-          similarity_boost: 1,
-          style_exaggeration: 0.2
+          similarity_boost: 0.8,
         },
       },
       responseType: "arraybuffer",
