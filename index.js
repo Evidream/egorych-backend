865c59da3b5c66f2c732e801c8a067c159d624d0
Evidream <e.v.i.dream@mail.ru>
2025-06-22 13:53:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,10 @@ const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://egorych.ai'],
+  credentials: true
+}));
 app.use(bodyParser.json({ limit: "10mb" }));
 
 // === UPLOAD ===
